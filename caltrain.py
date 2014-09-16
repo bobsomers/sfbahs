1d5bea11ed6b2e37261721177712be52a40032a6
Bob Somers <bob@bobsomers.com>
2014-09-15 23:27:10 -0700

    Scrape (and cache) listings and addresses.

--- caltrain.py
+++ caltrain.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python2
 
-import feedparser
-
 # Pulls in the RSS feed of housing posts from Craigslist (with a few options),
 # scrapes each listing page for its address, and uses the Google Distance
 # Matrix API to rank them by their walking distance to major Caltrain stations.
+
+# TODO: Actually use the Google Distance API, lol.
+
+import feedparser
+import HTMLParser
+import os.path
+import pickle
+import re
+import urllib2
 
 # Settings:
 numBathrooms = 2
@@ -13,6 +20,22 @@ minAsk = 2500 # in dollars
 maxAsk = 4000 # in dollars
 region = 'sby' # craigslist region, sby = south bay, pen = peninsula, etc.
 
+# Constant-ish stuff. No need to mess with it.
+addressRegex = re.compile('<div class="mapaddress">(.*)</div>')
+cityRegex = re.compile('&amp;csz=(.*)&amp;')
+listingCache = 'listing.cache'
+addressCache = 'address.cache'
+
+def dumpCache(obj, path):
+    with open(path, 'wb') as f:
+        pickle.dump(obj, f)
+
+def loadCache(path):
+    if os.path.isfile(path):
+        with open(path, 'rb') as f:
+            return pickle.load(f)
+    return None
+
 def buildListingsUrl(skip):
     url = 'http://sfbay.craigslist.org/search/{0}/apa?'.format(region)
     url += 'bathrooms={0}&bedrooms={1}'.format(numBathrooms, numBedrooms)
@@ -20,28 +43,77 @@ def buildListingsUrl(skip):
     url += '&s={0}&format=rss'.format(skip)
     return url
 
-def collectListings():
-    print('Collecting listings...')
+def scrapeListings():
+    print('Scraping listings...')
     skip = 0
-    lastContenderCount = 0
-    contenders = {}
+    lastListingCount = 0
+    listings = {}
     while True:
         url = buildListingsUrl(skip)
         feed = feedparser.parse(url)
 
         if len(feed['entries']) <= 0:
-            return contenders
+            return listings
         for entry in feed['entries']:
-            contenders[entry['link']] = entry['title']
+            listings[entry['link']] = entry['title']
         skip += len(feed['entries'])
         print(skip)
 
-        if len(contenders) == lastContenderCount:
+        if len(listings) == lastListingCount:
             break
-        lastContenderCount = len(contenders)
+        lastListingCount = len(listings)
 
-    print('Found {0} unique listings.'.format(len(contenders)))
-    return contenders
+    print('Fetched {0} unique listings.'.format(len(listings)))
+    return listings
+
+def loadListings():
+    listings = loadCache(listingCache)
+    if listings is None:
+        listings = scrapeListings()
+        dumpCache(listings, listingCache)
+    else:
+        print('Loaded {0} listings from cache.'.format(len(listings)))
+    return listings
+
+def scrapeAddress(url):
+    markup = urllib2.urlopen(url).read()
+    addressMatch = addressRegex.search(markup)
+    if not addressMatch:
+        return False
+    address = addressMatch.group(1).strip()
+    cityMatch = cityRegex.search(markup)
+    if not cityMatch:
+        return False
+    city = cityMatch.group(1).strip().replace('++', ', ').replace('+', ' ')
+    return '{0}, {1}'.format(address, city)
+
+def loadAddresses(listings):
+    addresses = loadCache(addressCache)
+    if addresses is None:
+        addresses = {}
+    else:
+        print('Loaded {0} addresses from cache.'.format(len(addresses)))
+
+    htmlParser = HTMLParser.HTMLParser()
+    curListing = 1
+    totalListings = len(listings)
+    if totalListings > 0:
+        print('Scraping addresses from listings...')
+
+    while len(listings) > 0:
+        (url, title) = listings.popitem()
+        print(u'[{0}/{1}] {2}'.format(curListing, totalListings, htmlParser.unescape(title)))
+        curListing += 1
+
+        address = scrapeAddress(url)
+        if address:
+            addresses[url] = {'title': title, 'address': address}
+
+        # This is horrendously inefficient, but Python is already slow, so who
+        # cares, right? Right? *crickets*
+        dumpCache(addresses, addressCache)
+        dumpCache(listings, listingCache)
 
 if __name__ == '__main__':
-    contenders = collectListings()
+    listings = loadListings()
+    addresses = loadAddresses(listings)
