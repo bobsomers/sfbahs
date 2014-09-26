c14d52a6675afd8199a703c199510c25e2c22490
AJ <aignacio724@gmail.com>
2014-09-26 02:09:49 -0700

    Integrated the Google Distance Matrix API.

--- caltrain.py
+++ caltrain.py
@@ -3,7 +3,7 @@
 # Pulls in the RSS feed of housing posts from Craigslist (with a few options),
 # scrapes each listing page for its address, and uses the Google Distance
 # Matrix API to rank them by their walking distance to major Caltrain stations.
-
+# AIzaSyDIe54Kx52-RdwIBxwQ07ZDUbuRs0U1AUc
 # TODO: Actually use the Google Distance API, lol.
 
 import feedparser
@@ -12,6 +12,8 @@ import os.path
 import pickle
 import re
 import urllib2
+import json as simplejson 
+import sys
 
 # Settings:
 numBathrooms = 2
@@ -21,10 +23,12 @@ maxAsk = 4000 # in dollars
 region = 'sby' # craigslist region, sby = south bay, pen = peninsula, etc.
 
 # Constant-ish stuff. No need to mess with it.
-addressRegex = re.compile('<div class="mapaddress">(.*)</div>')
-cityRegex = re.compile('&amp;csz=(.*)&amp;')
-listingCache = 'listing.cache'
-addressCache = 'address.cache'
+addressRegex   = re.compile('<div class="mapaddress">(.*)</div>')
+cityRegex      = re.compile('&amp;csz=(.*)&amp;')
+listingCache   = 'listing.cache'
+addressCache   = 'address.cache'
+distanceCache  = 'distance.cache'
+GOOGLE_API_BASE_URL = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins='
 
 def dumpCache(obj, path):
     with open(path, 'wb') as f:
@@ -122,10 +126,66 @@ def loadAddresses(listings):
         dumpCache(listings, listingCache)
     return addresses
 
+def formatAddress(addresses):
+    addressList = []
+    for v in addresses.values():
+        temp = re.sub('[.!,;-?-]', ' ',v['address'])
+        addressList.append(re.sub('\s+','+',temp)) 
+    return addressList
+
+def getDistances(addressList, destination, API_KEY):
+    jsonObj = loadCache(distanceCache)
+    #print destination
+    #print API_KEY
+    if jsonObj is None: 
+        result_org_address = []
+        result_rows = []
+
+        # Create first element in the json object
+        googleUrl = GOOGLE_API_BASE_URL 
+        googleUrl += '{0}|'.format(addressList[0])
+        googleUrl += '&destinations={0}'.format(destination)
+        googleUrl += '&mode=driving&units=imperial'
+        googleUrl += '&key={0}'.format(API_KEY)
+        #print googleUrl
+        jsonObj = simplejson.load(urllib2.urlopen(googleUrl))
+        
+        ''' This is really ugly and slow I know.  Would like to do 100 elements per query
+            but Google API URLs are restricted to 2000 chars and the addresses
+            are of varying lengths. So for now doing each address per query.'''
+        # TODO: Create chunking of the url query 
+        
+        # Only doing 50 at the moment (Hit my daily request quota
+        for x in addressList[1:50]: 
+            googleUrl = GOOGLE_API_BASE_URL 
+            googleUrl += '{0}|'.format(x)
+            googleUrl += '&destinations={0}'.format(destination)
+            googleUrl += '&mode=driving&units=imperial'
+            googleUrl += '&key={0}'.format(API_KEY)
+            result = simplejson.load(urllib2.urlopen(googleUrl))
+            result_org_address.append(result['origin_addresses'][0])
+            result_rows.append(result['rows'][0])
+
+        # Append all the addresses to the json object origins_address array
+        for i in result_org_address:
+            jsonObj['origin_addresses'].append(i)
+
+        # Append all the row data for each address to the json object
+        for i in result_rows:
+            jsonObj['rows'].append(i)
+        dumpCache(jsonObj, distanceCache)
+    else:
+        print simplejson.dumps(jsonObj,sort_keys=True,indent=4)
+
 if __name__ == '__main__':
     listings = loadListings()
     addresses = loadAddresses(listings)
+    addressList = formatAddress(addresses)
+    dest = sys.argv[1]
+    key = sys.argv[2]
+    dest = re.sub(r"\s+", '+', dest)
+    getDistances(addressList, dest, key) 
     
     # TODO: just print all the addresses and sqft for now
-    for v in addresses.values():
-        print(v['address'], v['sqft'])
+    #for v in addresses.values()
+    #    print(v['address'], v['sqft']) 
